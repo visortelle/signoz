56ee62b52fe13937193522e9b3d511a5a8cb9d8e
visortelle <visortelle@gmail.com>
2023-05-03 16:51:57 +0200

    Add QueryBuilder; Store tab in URL.

--- frontend/src/AppRoutes/index.tsx
+++ frontend/src/AppRoutes/index.tsx
@@ -9,6 +9,8 @@ import history from 'lib/history';
 import { QueryBuilderProvider } from 'providers/QueryBuilder';
 import React, { Suspense } from 'react';
 import { Route, Router, Switch } from 'react-router-dom';
+import { QueryParamProvider } from "use-query-params";
+import { ReactRouter5Adapter } from 'use-query-params/adapters/react-router-5';
 
 import PrivateRoute from './Private';
 import routes from './routes';
@@ -19,30 +21,32 @@ function App(): JSX.Element {
 	return (
 		<ConfigProvider theme={themeConfig}>
 			<Router history={history}>
-				<NotificationProvider>
-					<PrivateRoute>
-						<ResourceProvider>
-							<QueryBuilderProvider>
-								<AppLayout>
-									<Suspense fallback={<Spinner size="large" tip="Loading..." />}>
-										<Switch>
-											{routes.map(({ path, component, exact }) => (
-												<Route
-													key={`${path}`}
-													exact={exact}
-													path={path}
-													component={component}
-												/>
-											))}
+				<QueryParamProvider adapter={ReactRouter5Adapter}>
+					<NotificationProvider>
+						<PrivateRoute>
+							<ResourceProvider>
+								<QueryBuilderProvider>
+									<AppLayout>
+										<Suspense fallback={<Spinner size="large" tip="Loading..." />}>
+											<Switch>
+												{routes.map(({ path, component, exact }) => (
+													<Route
+														key={`${path}`}
+														exact={exact}
+														path={path}
+														component={component}
+													/>
+												))}
 
-											<Route path="*" component={NotFound} />
-										</Switch>
-									</Suspense>
-								</AppLayout>
-							</QueryBuilderProvider>
-						</ResourceProvider>
-					</PrivateRoute>
-				</NotificationProvider>
+												<Route path="*" component={NotFound} />
+											</Switch>
+										</Suspense>
+									</AppLayout>
+								</QueryBuilderProvider>
+							</ResourceProvider>
+						</PrivateRoute>
+					</NotificationProvider>
+				</QueryParamProvider>
 			</Router>
 		</ConfigProvider>
 	);
--- frontend/src/pages/TraceExplorer/index.tsx
+++ frontend/src/pages/TraceExplorer/index.tsx
@@ -1,7 +1,57 @@
-import React from 'react';
+import { Card, Col, Row, Tabs } from 'antd';
+import { QueryBuilder } from 'container/QueryBuilder';
+// import { useQueryBuilder } from 'hooks/queryBuilder/useQueryBuilder';
+import React, { useEffect } from 'react';
+import { createEnumParam } from 'serialize-query-params';
+import { DataSource } from 'types/common/queryBuilder';
+import { StringParam, useQueryParam, withDefault } from 'use-query-params';
+
+// eslint-disable-next-line sonarjs/no-duplicate-string
+const tabs = ['list', 'traces', 'time-series', 'table'] as const;
+type Tab = typeof tabs[number];
+const TabUrlParam = createEnumParam<Tab>([...tabs]);
 
 function TraceExplorer(): JSX.Element {
-	return <div>hello</div>;
+	// const { queryBuilderData, initQueryBuilderData } = useQueryBuilder();
+	const [tab, setTab] = useQueryParam('tab', withDefault(TabUrlParam, 'list'));
+
+	if (!tab) {
+		return null;
+	}
+
+	return (
+		<Card>
+			<Row>
+				<Col span={24}>
+					<QueryBuilder
+						panelType="EMPTY_WIDGET"
+						config={{ initialDataSource: DataSource.TRACES, queryVariant: 'static' }}
+					/>
+
+					<Tabs
+						style={{ marginTop: '4rem' }}
+						activeKey={tab}
+						onChange={(v): void => setTab(v as Tab)}
+						items={[
+							{ key: 'list', label: 'List View' },
+							{ key: 'traces', label: 'Traces' },
+							{ key: 'time-series', label: 'Time Series' },
+							{ key: 'table', label: 'Table View' },
+						]}
+					/>
+				</Col>
+			</Row>
+
+			<Row>
+				<Col span={24}>
+					{tab === 'list' && <div>list</div>}
+					{tab === 'traces' && <div>traces</div>}
+					{tab === 'time-series' && <div>time-series</div>}
+					{tab === 'table' && <div>table</div>}
+				</Col>
+			</Row>
+		</Card>
+	);
 }
 
 export default TraceExplorer;
